99f44ab2fa86cdebf83032e1e6f7df498d927ba2
occiandiaali <ocean.diaali@gmail.com>
2021-03-18 18:37:07 +0100

    remove secret creds

--- src/api/fetchWeather.tsx
+++ src/api/fetchWeather.tsx
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "";
 
 export const fetchWeather = async (query: any) => {
-    const { data } = await axios.get(URL, {
-        params: {
-            q: query,
-            units: 'metric',
-            APPID: API_KEY
-        }
-    });
-    return data;
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
 };
